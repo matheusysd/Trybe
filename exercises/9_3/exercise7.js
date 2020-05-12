994e510b63d4f61939b8cf1bc6ec5a14c107fe9f
matheusysd <matheusyurisd@gmail.com>
2020-05-12 19:39:15 -0300

    exercise 7

--- exercises/9_3/exercise7.js
+++ exercises/9_3/exercise7.js
@@ -2,24 +2,24 @@ const assert = require('assert')
 
 const ships = [
     {
-        name: "Titanic",
+        name: 'Titanic',
         length: 269.1
     },
     {
-        name: "Queen Mary 2",
+        name: 'Queen Mary 2',
         length: 1132,
-        measurementUnit: "feet"
+        measurementUnit: 'feet'
     },
     {
-        name: "Yamato",
+        name: 'Yamato',
         length: 256,
-        measurementUnit: "meters"
+        measurementUnit: 'meters'
     }
 ]
 
 const shipLength = ({name, length, measurementUnit = 'meters'}) => `${name} is ${length} ${measurementUnit} long`;
 
-assert.equal(shipLength(ships[0]), "Titanic is 269.1 meters long")
-assert.equal(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
-assert.equal(shipLength(ships[2]), "Yamato is 256 meters long")
+assert.equal(shipLength(ships[0]), 'Titanic is 269.1 meters long')
+assert.equal(shipLength(ships[1]), 'Queen Mary 2 is 1132 feet long')
+assert.equal(shipLength(ships[2]), 'Yamato is 256 meters long')
     
