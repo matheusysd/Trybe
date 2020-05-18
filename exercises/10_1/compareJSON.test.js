eca0c96ee11b131e64a5c4f91eefb4b6156ff840
matheusysd <matheusyurisd@gmail.com>
2020-05-18 19:50:00 -0300

    compareJSON test

---
+++ exercises/10_1/compareJSON.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+test("comparing objects", () => {
+  expect(obj1).not.toEqual(obj2);
+  expect(obj2).not.toEqual(obj3);
+  expect(obj1).not.toEqual(obj3);
+});
